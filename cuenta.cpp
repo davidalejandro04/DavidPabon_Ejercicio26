46e255d5afb2c98e5c116f1a2e9fdad33d1ce3cd
davidalejandro04 <da.pabon10@uniandes.edu.co>
2018-11-08 23:48:26 -0500

    Ya calcula tiempo y cuenta strings

--- cuenta.cpp
+++ cuenta.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
 
 using namespace std;
 
 char* nombreArchivo;
 char* nombreCuenta;
 char* nombreTiempo;
+
+int contarString(const std::string& str, const std::string& sub);
 
 
 int main(int argc, char** argv)
@@ -14,14 +17,8 @@ int main(int argc, char** argv)
 	nombreCuenta=argv[2];
 	nombreTiempo=argv[3];
 
-	std::cout <<nombreArchivo<<", "<<nombreCuenta<<", "<<nombreTiempo<<"\n";
+	clock_t begin = clock();
 
-
-
-	ofstream cuentatxt;
-	cuentatxt.open (nombreCuenta);
-	cuentatxt << "Probando.\n";
-	cuentatxt.close();
 
 	string line;
 	string txt;
@@ -33,11 +30,44 @@ int main(int argc, char** argv)
 			txt=txt+ line;
 		}
 		archivotxt.close();
-		cout<<txt;
 	}
-
 	else cout << "No se pudo abrir el archivo\n"; 
+
+	string a="1";
+	string b="12";
+	string c="123";
+	string d="1234";
+	string e="12345";
+
+
+	ofstream cuentatxt;
+	cuentatxt.open (nombreCuenta);
+
+	cuentatxt<<contarString(txt,a)<<"\n"<<contarString(txt,b)<<"\n"<<contarString(txt,c)<<"\n"<<contarString(txt,d)<<"\n"<<contarString(txt,e)<<"\n";
+
+	cuentatxt.close();
+
+	ofstream tiempotxt;
+	tiempotxt.open(nombreTiempo);
+
+	clock_t end = clock();
+	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	tiempotxt<<elapsed_secs<<"\n";
+	tiempotxt.close();
 
 	return 0;
 
+}
+
+
+int contarString(const std::string& str, const std::string& sub)
+{
+    if (sub.length() == 0) return 0;
+    int count = 0;
+    for (size_t offset = str.find(sub); offset != std::string::npos;
+	 offset = str.find(sub, offset + sub.length()))
+    {
+        ++count;
+    }
+    return count;
 }
